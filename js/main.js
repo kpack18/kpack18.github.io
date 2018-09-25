00cb96b42ab440023c4ab26dd21a7d69bb4fd5a0
tadari <tgadari@buffalo.edu>
2018-09-25 17:30:56 -0400

    Drawing/Erasing of walls
    Added the ability to create/delete walls by dragging the mouse over the tiles. A blank tile becomes a wall, and vice versa.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 var current_color = 'black';
+var old_crd = 0;
+
 
 function set_Wall(elem) {
-  if(elem.style.backgroundColor == 'black'){
-    elem.style.backgroundColor = 'white';
-  }
-  else{
-    elem.style.backgroundColor = 'black';
-  }
-}
+	var curr_crd = elem.getBoundingClientRect().top + window.scrollY + elem.getBoundingClientRect().right;
+	
+	$(elem).mousemove(function(e){
+		
+		if(e.which == 1 && old_crd != curr_crd){
+			
+			old_crd = curr_crd;
+
+			if(elem.style.backgroundColor == 'black'){
+				
+				elem.style.backgroundColor = 'white';
+				
+			}
+			else{
+				
+				elem.style.backgroundColor = 'black';	
+				
+			}
+		}
+	});
+ }
 
 function set_Color(elem){
   elem.style.backgroundColor = current_color;

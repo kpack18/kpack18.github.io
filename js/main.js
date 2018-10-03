9d58eeff4bf1d45024c2e5d6c8a6cc23647914f5
Kyle <kylekolp@buffalo.edu>
2018-10-02 20:12:16 -0400

    Created Classes for Grid, Tiles, and Palette(color selection) so we can more easily change the state of our html elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,94 @@
-var current_color = 'black';
-var old_crd = 0;
+// Grid Class: Stores the 2 Dimensional Matrix of Tiles and functions to edit them
+class Grid {
+/*  Vars: length: Length of the Grid Tiles
+          Width: Width of the Grid Tiles
+
+        grid: 2 Dimensional Array Housing the Tile Classes for each tile
+           - Initialized to a L x W matrix of null values till addTile is called
+
+        size_L: Keeps track of the next available length in the grid
+        size_W: Keeps track of the next available width index in the grid
+            -Tile is added at grid[size_L][size_W] till grid is full */
+  constructor(l,w){
+    this.length = l;
+    this.width = w;
+    this.grid = [];
+
+    this.size_L = 0;
+    this.size_W = 0;
+
+    for(i = 0; i < l; ++i){
+      var temparray = [];
+      for(j = 0; j < w; ++i){
+        temparray.push(null);
+      }
+      this.grid.push(temparray);
+    }
+  }
+/*  addTile (PARAM) tile: tile class to be added to the grid
+                  : tile class is added at index grid[size_L][size_W],
+                    then size_L and W are incremented */
+  addTile(tile){
+    if(this.size_L == this.length && this.size_W == this.width){ return; }
+    this.grid[size_L][size_W] = tile;
+    ++this.size_L;
+
+    if(this.size_L == this.length){
+      this.size_L == 0;
+      ++this.size_W;
+    }
+  }
+/*  getTile (PARAM) xcoor: x coordinate of tile in Grid
+          (PARAM) ycoor: y coordinate of tile in Grid
+      : returns the tile class at grid[xcoor][ycoor] */
+  getTile(xcoor,ycoor){
+    if(xcoor >= this.length || ycoor >= this.width){ return; }
+    return this.grid[xcoor,ycoor];
+  }
+
+}
+
+//Tile Class: Houses the element of a tile, it's coordinates within the grid, and functions to edit it.
+class Tile {
+  constructor(elem,xcoor,ycoor){
+/* Vars: element: The html element of the tile
+         x, y: The x and y coordinates of the tile within the grid class */
+    this.element = elem;
+    this.x = xcoor;
+    this.y = ycoor;
+  }
+/* getX returns the x coordinate of the tile in grid */
+  getX(){
+    return this.x;
+  }
+/* getY returns the y coordinate of the tile in grid */
+  getY(){
+    return this.y;
+  }
+/* Set's the color of the html element to the current paint color */
+  setColor(color){
+    this.element.css('backgroundColor', palette.getPaint());
+  }
+}
+
+/* Palette Class: Hold's the current paint color and data structures
+                 for user defined color weights */
+class Palette {
+  constructor(){
+/* vars: paint_color: The current color of the brush.
+                      When a tile is clicked it'll change to current color */
+    this.paint_color = 'black';
+  }
+/* setPaint: (PARAM) color: new color of the Brush
+    : set's the color of the brush to "color" */
+  setPaint(color){
+    this.paint_color = color;
+  }
+// getPaint: Returns the current color of the brush
+  getPaint(){
+    return this.paint_color;
+  }
+}
 
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
@@ -11,7 +100,8 @@ $(document).ready(function () {
     });
 
     $('#color-pick').change(function () {
-        current_color = $(this).val();
+        //current_color = $(this).val();
+        palette.setPaint($(this).val());
     });
 
     $('.tile').click(function () {
@@ -25,6 +115,10 @@ $(document).ready(function () {
     $('#btnAlgorithm').click(myAlert);
 });
 
+var old_crd = 0;
+
+var palette = new Palette();
+
 function setWall(elem, e) {
     var curr_crd = elem[0].getBoundingClientRect().top + window.scrollY + elem[0].getBoundingClientRect().right;
 	if(e.which == 1 && old_crd != curr_crd){
@@ -34,7 +128,7 @@ function setWall(elem, e) {
  }
 
 function setColor(elem) {
-    elem.css('backgroundColor', current_color);
+    elem.css('backgroundColor', palette.getPaint());
 }
 
 function myAlert() {

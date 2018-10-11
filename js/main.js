313ffedb833ffd48c93f11a55d2766aab23e4dfe
tadari <tgadari@buffalo.edu>
2018-10-11 00:49:16 -0400

    Merge branch 'Visualize_Algo' of https://github.com/kpack18/Path_Visualizer into Select_Algo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var old_crd = 0;
 
-var grid = new Grid(8,8);
+var grid = new Grid(3,3);
 var palette = new Palette();
 
 $(document).ready(function () {
@@ -22,6 +22,7 @@ $(document).ready(function () {
         grid.getWeights();
     });
 
+<<<<<<< HEAD
     $('.btn').click(function () {
 	  var algoBarVal = document.getElementById("algo_select");
 	  var selected_algo = algoBarVal.value;
@@ -29,6 +30,12 @@ $(document).ready(function () {
       grid.clearPaths();
       var algorithm = new Algorithm(selected_algo); 
       var path = algorithm.run(grid.getTile(0,0),grid.getTile(0,7),grid); //Will Return a List containing the shortest path from  (0,0) to (0,6)
+=======
+    $('#algo').click(function () {
+      grid.clearPaths();
+      var algorithm = new Algorithm("bfs");
+      var path = algorithm.run(grid.getTile(0,0),grid.getTile(0,2),grid); //Will Return a List containing the shortest path from  (0,0) to (0,6)
+>>>>>>> a91ea36e715627fe0273e5e5c69e9df1494a7ed9
       console.log("path: " + printPath(path));
     });
 

cfe412bb7a7b50e7f54edec474f393f864c410ba
Danealor <iamblonder@gmail.com>
2018-10-13 00:09:56 -0400

    Implement loading from saved file (waiting on resize grid)

--- js/datafile.js
+++ js/datafile.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
     $('#save').click(save);
+    $('#load').click(function () {
+        $('#infile').click();
+    });
+    $('#infile').change(function () {
+        load(this.files[0]);
+    });
 });
 
+var header = textToData("!ALGORITHM_VISUALIZER!");
+var reader = new FileReader();
+
+reader.onloadend = function (evt) {
+    var contents = evt.target.result;
+    var error = evt.target.error;
+
+    if (error === null && contents instanceof ArrayBuffer) {
+        var arr = new Uint8Array(contents);
+        parseData(arr);
+    }
+};
+
+function load(file) {
+    reader.readAsArrayBuffer(file);
+}
+
 function save() {
-    var header = textToData("!ALGORITHM_VISUALIZER!");
     var data = generateData(grid, palette);
     var blob = new Blob([header, data]);
     var date = new Date();
@@ -24,6 +46,10 @@ function rgbToData(rgb) {
     return rgb
         .match(/^\d+|\d+\b|\d+(?=\w)/g)
         .map(function (v) { return +v; });
+}
+
+function dataToRgb(red, green, blue) {
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 function generateData(grid, palette) {
@@ -54,8 +80,8 @@ function generateData(grid, palette) {
     }
 
     // Next four bytes are length x width of grid
-    view.setUint16(i, length); i += 2;
-    view.setUint16(i, width); i += 2;
+    view.setUint16(i, length, false); i += 2;
+    view.setUint16(i, width, false); i += 2;
 
     // Next length * width bytes are the grid of weight ID's
     for (var x = 0; x < length; x++) {
@@ -68,4 +94,40 @@ function generateData(grid, palette) {
     }
 
     return arr;
+}
+
+function parseData(arr) {
+    var view = new DataView(arr.buffer);
+
+    // Check header
+    for (var i = 0; i < header.length; i++) {
+        if (arr[i] !== header[i]) return;
+    }
+
+    // First byte is number of weights
+    var weights = arr[i++];
+
+    // Next 'weights' * 7 bytes are the weights' colors
+    palette.weight_list = new Map();
+    for (var w = 0; w < weights; w++) {
+        var red = arr[i++];
+        var green = arr[i++];
+        var blue = arr[i++];
+        var weight = view.getInt32(i, false); i += 4;
+        palette.weight_list.add(dataToRgb(red, green, blue), weight);
+    }
+
+    // Next four bytes are length x width of grid
+    var length = view.getUint16(i, false); i += 2;
+    var width = view.getUint16(i, false); i += 2;
+
+    // Next length * width bytes are the grid of weight ID's
+    // DEPENDENT ON DATAFYING BRANCH: resize grid to length x width
+    for (var x = 0; x < length; x++) {
+        for (var y = 0; y < width; y++) {
+            var id = arr[i++];
+            var color = palette.weight_list.map[id].key;
+            grid.getTile(x, y).setColor(color);
+        }
+    }
 }

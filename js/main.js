dc857e73c416d95459b3d6029a7fad2b61e30fff
lieric4567 <43122690+lieric4567@users.noreply.github.com>
2018-10-15 21:10:43 -0400

    Added resizing of the background width and height
    dynamically changes the grid-containers width and height for fade effect

--- js/main.js
+++ js/main.js
@@ -41,6 +41,10 @@ var old_crd = 0;
 let grid = new Grid(8,8);
 var palette = new Palette();
 var apply_button = document.getElementById("apply_btn");
+$('#grid-container').css({
+    'width':807 + 'px' ,'height':807 + 'px'
+});
+
 apply_button.addEventListener("click", resizeGrid);
 
 function setWall(elem, e) {
@@ -58,6 +62,9 @@ function resizeGrid(){
     var grid_width = document.getElementById("grid-width").value;
     var grid_height = document.getElementById("grid-height").value;
     var total = grid_width * grid_height;
+    let total_width = 0;
+    let total_height = 0;
+
     if(grid_height == 0 || grid_width == 0 || grid_height < 0 || grid_width < 0){
         return;
     }
@@ -68,18 +75,44 @@ function resizeGrid(){
         switch (true){
             case (total >= 5000):
                 document.documentElement.style.setProperty("--size", "20px");
+                total_width = (grid_width * 20) + (1 * grid_width);
+                total_height = (grid_height * 20) + (1 * grid_height);
+                //sets the css style width and height
+                $('#grid-container').css({
+                    'width':total_width + 'px' ,'height':total_height + 'px'
+                });
                 break;
             case (total >= 2500):
                 document.documentElement.style.setProperty("--size", "30px");
+                total_width = (grid_width * 30) + (1 * grid_width);
+                total_height = (grid_height * 30) + (1 * grid_height);
+                $('#grid-container').css({
+                    'width':total_width + 'px' ,'height':total_height + 'px'
+                });
                 break;
             case (total >= 1000):
                 document.documentElement.style.setProperty("--size", "50px");
+                total_width = (grid_width * 50) + (1 * grid_width);
+                total_height = (grid_height * 50) + (1 * grid_height);
+                $('#grid-container').css({
+                    'width':total_width + 'px' ,'height':total_height + 'px'
+                });
                 break;
             case (total >= 100):
                 document.documentElement.style.setProperty("--size", "70px");
+                total_width = (grid_width * 70) + (1 * grid_width);
+                total_height = (grid_height * 70) + (1 * grid_height);
+                $('#grid-container').css({
+                    'width':total_width + 'px' ,'height':total_height + 'px'
+                });
                 break;
             default:
                 document.documentElement.style.setProperty("--size", "100px");
+                total_width = (grid_width * 100) + (1 * grid_width);
+                total_height = (grid_height * 100) + (1 * grid_height);
+                $('#grid-container').css({
+                    'width':total_width + 'px' ,'height':total_height + 'px'
+                });
                 break; 
         }
         for(var row = 0; row < grid.getLength(); ++row){
@@ -89,6 +122,7 @@ function resizeGrid(){
         }
         document.documentElement.style.setProperty("--width", grid_width);
         document.documentElement.style.setProperty("--height", grid_height);
+        document.getElementById("grid-container").style.maxWidth
         grid = new Grid(grid_height, grid_width);
 
         $('.tile').click(function () {
